f0988d3d9ee82588f75ab7a2d710ba1101b7f4be
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2025-04-06 21:42:21 +0200

    Add core classes for calculator, product, recipe, and user service functionality

---
+++ src/Test/UserServiceSimpleTest.java
@@ -0,0 +1,16 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserServiceSimpleTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	
+
+}
